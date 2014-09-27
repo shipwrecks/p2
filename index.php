5e7b7301c9f11a96bb0a583e19044d6099509b46
Teri Chu <terichu@fas.harvard.edu>
2014-09-27 15:47:12 -0400

    added real code into index, logic and style files

--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+error_reporting(E_ALL);   	# Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-	<meta charset="UTF-8">
-	<title>P2 - The xkcd Password Generator</title>
-	<?php require 'logic.php'; ?>
+    <head>
+    <meta charset="UTF-8">
+    <title>P2 - The xkcd Password Generator</title>
+    <?php require 'logic.php'; ?>
+    <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 
 <body>
-	<h1>xkcd Password Generator</h1>
-	<form method='GET' action='index.php'>
-		<input type='text' name='amount'><br>
-		<input type='submit' value='password please'><br>
-	</form>
+    <h1>xkcd Password Generator</h1>
+    <p>
+   	 I am a password generator.  Please fill out your requirements below, click on the
+   	 generate button, and I'll create a password for you to use.
+    </p>
+
+    <form method="GET" action="index.php">
+   	 <label name="count">How many words? (max 7)</label>
+   	 <input type="text" id="count" name="count" /><br>
+
+   	 <label name="number">Include a number?</label>
+   	 <input type="checkbox" id="number" name="number" /><br>
+
+   	 <label name="symbol">Include a symbol?</label>
+   	 <input type="checkbox" id="symbol" name="symbol" /><br>
+
+   	 <input type="submit" value="generate!"><br>
+    </form>
+
+    <h2>Your new password is: </h2>
 </body>
 
 </html>
+
--- logic.php
+++ logic.php
@@ -19,4 +19,48 @@ $words = array (
 	'coriander',
 	);
 
+$symbols = array (
+	'!',
+	'@',
+	'#',
+	'$',
+	'&',
+	'*',
+	);
+
+$numbers = array (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
 print_r ($words);
+
+
+if (isset($_GET['count'])) {
+    $count = $_GET['count'];
+} 
+
+if (isset($_GET['number'])) {
+    $number = true;
+} else {
+    $number = false;
+}
+
+if (isset($_GET['symbol'])) {
+    $symbol = true;
+} else {
+    $symbol = false;
+}
+
+$selection = array(); //empty array
+
+$max = count($words); //number of elements in original array
+if ($count>0 AND $count<8) {
+	for ($i = 0; $i < $count; $i++) {
+	    $pick = rand(0, $max-1);
+	    $word = $words[$pick];
+	    echo $pick.' of '.$max; //just for testing
+	    echo $word.'<br />';  //just for testing
+	    array_push[$selection, $word];
+	}
+} else {
+	echo 'You did not submit a valid number of words.';
+}
+
